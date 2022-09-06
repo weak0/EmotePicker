e9fb80319016f59c12dcec402ec935218f4977bb
Maciej Gorzela <Maciej.Gorzela89@gmail.com>
2022-09-06 20:11:15 +0200

    first commit

---
+++ EmojiPicker.js
@@ -0,0 +1,145 @@
+class EmojiPicker {
+
+emojiTab = ['😀','😆','🤣','😉','😛','🤑','😪']
+emojisBtns = null
+
+
+constructor(btn, input) {
+
+    this.btn = document.querySelector(btn)
+    this.input = document.querySelector(input)
+    this.init()
+    
+
+}
+
+
+  init() {
+
+
+
+
+
+    this.btn.addEventListener( 'click', () => this.openPicker())
+    document.addEventListener('click' , (e) => this.closePicker(e))
+    
+
+
+  }  
+
+
+  closePicker(e) {
+
+    const picker = document.querySelector('#emotePicker')
+  
+    if(picker !== null && !e.target.classList.contains('emoji') && e.target !== this.btn  ) {
+
+        picker.remove()
+        
+    }
+
+
+  }
+
+  addListeners() {
+
+    
+    this.emojisBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => this.addEmote(e))
+    })
+
+ 
+
+    
+
+
+  }
+
+
+  openPicker() {
+
+
+    const picker = document.querySelector('#emotePicker')
+
+    if(  picker === null) {
+
+    const div = document.createElement('div')
+    div.id = 'emotePicker'
+    this.stylePicker(div);
+    this.generateEmoji(div);
+
+
+    const parentNode = this.btn.parentElement
+    parentNode.appendChild(div)
+    this.emojisBtns = document.querySelectorAll('.emoji')
+    this.addListeners()
+
+    }else {
+
+        picker.remove()
+        
+
+    }
+
+  }
+
+
+
+  stylePicker(div) {
+
+
+    const Y = this.btn.offsetTop + this.btn.offsetHeight
+    const X = this.btn.offsetLeft + this.btn.offsetWidth
+
+    div.style.position = 'fixed'
+    div.style.top = `${Y}px `
+    div.style.left = `${X}px`
+    div.style.width = '170px'
+    div.style.border ='1px solid black'
+    div.style.borderRadius = '20px'
+    div.style.display = 'flex'
+    div.style.flexWrap = 'wrap'
+
+
+
+
+  }
+
+generateEmoji(div) {
+
+
+   this.emojiTab.forEach( emote => {
+
+    const emoteDiv = document.createElement('div')
+    emoteDiv.setAttribute('class', 'emoji')
+    emoteDiv.textContent = emote
+    emoteDiv.style.margin = '10px'
+    emoteDiv.style.cursor = 'pointer'
+    div.appendChild(emoteDiv)
+
+
+   })
+
+}
+
+
+addEmote = (e) => {
+
+const emote = e.target.textContent;
+this.input.value += emote
+
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+const emojiPicker = new EmojiPicker('[data-button]', '[data-textarea]')
